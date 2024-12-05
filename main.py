59ec2b776ede468b1e41e45f70829a5ac6c72f01
laiamanda <lai.amanda27@gmail.com>
2024-12-05 11:33:31 -0500

    Add a basic request

--- main.py
+++ main.py
@@ -3,8 +3,15 @@
 # Amanda L.
 # Version 1.0
 # 
+
+import requests
+
 class main():
-  print('Hello World')
+  print('Running web-scrapper-orgs')
+  # TO DO: Make a request to a website
+  site = requests.get('https://www.geeksforgeeks.org/python-programming-language/')
+  # print(site)
+  print(site.content)
 
 if __name__ == '__main__':
   main()
