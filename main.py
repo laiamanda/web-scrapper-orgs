635e4b9ea23ce846ded74082377eff4564078e07
laiamanda <lai.amanda27@gmail.com>
2024-12-05 12:18:18 -0500

    Add BeautifulSoup to parse html

--- main.py
+++ main.py
@@ -5,13 +5,28 @@
 # 
 
 import requests
+from bs4 import BeautifulSoup
 
 class main():
   print('Running web-scrapper-orgs')
-  # TO DO: Make a request to a website
-  site = requests.get('https://www.geeksforgeeks.org/python-programming-language/')
-  # print(site)
-  print(site.content)
+
+  url = 'https://www.geeksforgeeks.org/python-programming-language/'
+
+  # Make a request to a website
+  site = requests.get(url)
+  
+  # If the request failed, then program terminates
+  if site.status_code != 200:
+    print('[ERROR] Request failed')
+    exit(0)
+
+  # Use BeautifulSoup to parse the HTML
+  soup = BeautifulSoup(site.content, 'html.parser')
+  # print(soup.prettify(formatter="html").encode('utf-8'))
+  s = soup.find('div', class_='entry-content')
+  content = soup.find_all('p')
+
+  print(content)
 
 if __name__ == '__main__':
   main()
