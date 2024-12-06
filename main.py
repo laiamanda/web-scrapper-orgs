25cebbf533ed2084b4922ec61450173893340e02
laiamanda <lai.amanda27@gmail.com>
2024-12-06 10:56:20 -0500

    BeautifulSoup is working again

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from bs4 import BeautifulSoup
 class main():
   print('Running web-scrapper-orgs')
 
-  url = 'https://www.geeksforgeeks.org/python-programming-language/'
+  url = 'https://realpython.github.io/fake-jobs/'
 
   # Make a request to a website
   site = requests.get(url)
@@ -20,13 +20,25 @@ class main():
     print('[ERROR] Request failed')
     exit(0)
 
+  # print(site.text)
+
   # Use BeautifulSoup to parse the HTML
   soup = BeautifulSoup(site.content, 'html.parser')
-  # print(soup.prettify(formatter="html").encode('utf-8'))
-  s = soup.find('div', class_='entry-content')
-  content = soup.find_all('p')
+  results = soup.find(id='ResultsContainer')
+  # print(results.prettify())
+  # content = soup.find_all('p')
 
-  print(content)
+  job_cards = results.find_all('div', class_='card-content')
+
+  for job_card in job_cards:
+    title_element = job_card.find("h2", class_="title")
+    company_element = job_card.find("h3", class_="company")
+    location_element = job_card.find("p", class_="location")
+    
+    print(title_element)
+    print(company_element)
+    print(location_element)
+
 
 if __name__ == '__main__':
   main()
