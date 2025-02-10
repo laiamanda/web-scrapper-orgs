052542659ab352c1f6e8448e556e350795afff8d
laiamanda <lai.amanda27@gmail.com>
2025-02-10 14:15:44 -0500

    Able to find the orgs.txt file

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ class main():
   print('Running web-scrapper-orgs')
 
   fileHandling.findOrgsFile()
-
   # fileHandling.insertIntoFile()
 
   # # url = 'https://realpython.github.io/fake-jobs/'
@@ -37,31 +36,10 @@ class main():
   # try: 
   #   for page in pages:
   #     text = page.get_text().encode('utf-8')
-  #     # TO DO: Find a way to decode it but allowing all character to pass thur from multiple different languages
-  #     # that's not supported by
   #     # Manually insert it into a text file and configure the names from there, because unable to decode it
   #     print(text)
   # except Exception as error :
   #   print(error)
-    
-  ### DEVELOPMENT ###
-  # Use BeautifulSoup to parse the HTML
-  # soup = BeautifulSoup(site.content, 'html.parser')
-
-  # results = soup.find(id='ResultsContainer') # id mw-pages 
-  # # print(results.prettify())
-
-  # job_cards = results.find_all('div', class_='card-content')
-
-  # for job_card in job_cards:
-  #   title_element = job_card.find("h2", class_="title")
-  #   company_element = job_card.find("h3", class_="company")
-  #   location_element = job_card.find("p", class_="location")
-    
-  #   print(title_element)
-  #   print(company_element)
-  #   print(location_element)
-
 
 if __name__ == '__main__':
   main()
--- util/fileHandling.py
+++ util/fileHandling.py
@@ -1,9 +1,21 @@
-# import fs
+import os
+import glob
 
+# Retrieve the location path of the main directory - Not Being Used
+def getMainDirectory():
+    # Retrieve the main Directory where the script is being ran at
+    mainDirectory = os.path.abspath(os.getcwd())
+
+# Search for the Org File
 def findOrgsFile():
-    print("Searching")
+    # Goes through every file and directories to see if the file exists
+    if(os.path.exists("orgs.txt")): 
+        return
+    else :
+        f = open("orgs.txt", "x")
+        f.close()
 
 def insertIntoFile():
     test = ['a', 'b', 'c']
     for value in test:
-        print(value)
+        print(value) 
